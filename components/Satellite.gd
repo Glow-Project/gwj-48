fcb07c58639710c30f479c418bead7cf381df832
Tch1b0 <Jpour3006@gmail.com>
2022-08-14 20:52:09 +0200

    add satellite

---
+++ components/Satellite.gd
@@ -0,0 +1,19 @@
+extends Spatial
+
+export var target_path: NodePath
+export var speed: float = 0.1
+
+var center_radius: float
+var angle: float = 0.0
+
+onready var target: Spatial = get_node(target_path)
+
+func _ready():
+	center_radius = global_translation.distance_to(target.global_translation)
+
+func _physics_process(delta):
+	var pos = global_translation
+	var point = target.global_translation
+	angle += delta * speed
+	global_translation = point + (pos - point).rotated(point.normalized(), delta * speed)
+	$Camera.look_at(target.global_translation, Vector3.UP)
